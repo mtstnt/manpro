cbbbc23ddddc01337b855414b41ca646f2977c00
mtstnt <matthew.sutanto0612@yahoo.com>
2022-04-03 21:57:24 +0700

    requirements.txt in web

--- web/Server.py
+++ web/Server.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/upload', method=['POST'])
+@app.route('/upload', methods=['POST'])
 def upload():
     req_data_client = request.files.get("client_data")
     req_data_driver = request.files.get("driver_data")
